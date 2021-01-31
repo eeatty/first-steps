50d96d42972c69d0bfd70e0b42d11dd4f60b2470
Emily <78229542+eeatty@users.noreply.github.com>
2021-01-31 11:23:57 -0500

    Create trying-js.js
    trying js code from beginning web dev

---
+++ trying-js.js
@@ -0,0 +1,38 @@
+//Changable Variables
+let var1 
+var1 = 123;
+var1
+
+//Constant Variables
+const VAR2 = 3;
+
+//Calculations
+1 + 2 
+1 - 2
+1 * 3
+3 / 4
+3 % 4
+
+//Strings
+"This is a string"
+'This, too, is a string'
+let myString = 'string stored in a variable';
+
+//Using strings
+let st1 = "Good";
+let st2 = "to go";
+st1 + st2;
+st1 + " " + st2;
+st1 + " " + st2 + "?";
+
+//Template literals
+`${st1} ${st2}`
+`${st1} ${st2}!`
+
+//Booleans - true or false
+let myTruBoo = true
+let myFlsBoo = false
+myTruBoo //is truthy
+myFlsBoo //is falsy
+
+//JS - CAPTIALIZATION MATTERS
