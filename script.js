bcdae16578c05839cadd55c51cca613bdfc4cc66
Emily <78229542+eeatty@users.noreply.github.com>
2021-02-26 13:31:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ let startTime = Date.now();
 const quoteElement = document.getElementById('quote');
 const messageElement = document.getElementById('message');
 const typedValueElement = document.getElementById('typed-value');
+
+document.getElementById('start').addEventListener('click', () => {
+    const quoteIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[quoteIndex];
+    words = quote.split(' ');
+    wordIndex = 0;
+
+    const spanWords = words.map(function(word) { return `<span>${word} </span>`});
+    quoteElement.innerHTML = spanWords.join('');
+    quoteElement.childNodes[0].className = 'highlight';
+    messageElement.innerText = '';
+    
+    typedValueElement.value = '';
+    typedValueElement.focus();
+    
+    startTime = new Date().getTime();
+});
+    
